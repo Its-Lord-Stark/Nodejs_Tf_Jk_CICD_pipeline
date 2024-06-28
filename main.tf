01dc814f9210b497af08665144b985696812342f
Stark <karneatharv1920@gmail.com>
2024-06-28 19:27:08 +0530

    Added Authorization token permission

--- main.tf
+++ main.tf
@@ -40,7 +40,8 @@ resource "aws_iam_role_policy" "ecr_policy" {
           "ecr:PutImage",
           "ecr:InitiateLayerUpload",
           "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload"
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken"
         ],
         "Resource": "${aws_ecr_repository.my_ecr_repo.arn}"
       }
